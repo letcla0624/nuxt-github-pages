9f2e2b45694e961571d69769cee43ddf56b30776
letcla0624 <letcla0624@yahoo.com.tw>
2023-01-07 13:31:44 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  // router: {
+  //   base: '/nuxt-github-pages/'
+  // }
+  app: {
+    baseURL: '/nuxt-github-pages/'
+  }
+})
